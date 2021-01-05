ac57d64e301fa0e633b4a0cec35e48c3c6d03b73
TOLIVEIN <TOLIVEIN@outlook.com>
2021-01-05 09:14:46 +0800

    fix(*) model

--- database/model.go
+++ database/model.go
@@ -1,18 +1,21 @@
 package database
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 //User ...
 type User struct {
-	gorm.Model `json:"id,createdAt,updatedAt,deletedAt"`
-	// ID        uint `gorm:"primarykey"`
-	// CreatedAt time.Time
-	// UpdatedAt time.Time
-	Username string `validate:"required,min=1,max=20" json:"username"`
-	Password string `validate:"required,min=6" json:"password"`
-	Email    string `validate:"required,email" json:"email"`
+	// gorm.Model `json:"id,createdAt,updatedAt,deletedAt"`
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Username  string         `validate:"required,min=1,max=20" json:"username"`
+	Password  string         `validate:"required,min=6" json:"password"`
+	Email     string         `validate:"required,email" json:"email"`
 }
 
 //Auth ...
@@ -24,28 +27,30 @@ type Auth struct {
 
 //Article ...
 type Article struct {
-	gorm.Model `json:"id,createdAt,updatedAt,deletedAt"`
-	// ID        uint `gorm:"primarykey"`
-	// CreatedAt time.Time
-	// UpdatedAt time.Time
+	// gorm.Model `json:"id,createdAt,updatedAt,deletedAt"`
 	// TagIDs        []uint
-	Title         string `validate:"required,max=100" json:"title"`
-	Description   string `validate:"max=255" json:"description"`
-	CoverImageURL string `validate:"required,max=255" json:"coverImageURL"`
-	Content       string `json:"content"`
-	CreatedBy     string `validate:"max=20" json:"createdBy"`
-	UpdatedBy     string `json:"updatedBy"`
-	Tags          []*Tag `gorm:"many2many:article_tag" json:"tags"`
+	ID            uint           `gorm:"primarykey" json:"id"`
+	CreatedAt     time.Time      `json:"createdAt"`
+	UpdatedAt     time.Time      `json:"updatedAt"`
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
+	Title         string         `validate:"required,max=100" json:"title"`
+	Description   string         `validate:"max=255" json:"description"`
+	CoverImageURL string         `validate:"required,max=255" json:"coverImageURL"`
+	Content       string         `json:"content"`
+	CreatedBy     string         `validate:"max=20" json:"createdBy"`
+	UpdatedBy     string         `json:"updatedBy"`
+	Tags          []*Tag         `gorm:"many2many:article_tag" json:"tags"`
 }
 
 //Tag ...
 type Tag struct {
-	gorm.Model `json:"id,createdAt,updatedAt,deletedAt"`
-	// ID        uint `gorm:"primarykey"`
-	// CreatedAt time.Time
-	// UpdatedAt time.Time
-	Name      string     `validate:"required,max=20" json:"name"`
-	CreatedBy string     `validate:"max=20" json:"createdBy"`
-	UpdatedBy string     `json:"updatedBy"`
-	Articles  []*Article `gorm:"many2many:article_tag" json:"articles"`
+	// gorm.Model `json:"id,createdAt,updatedAt,deletedAt"`
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Name      string         `validate:"required,max=20" json:"name"`
+	CreatedBy string         `validate:"max=20" json:"createdBy"`
+	UpdatedBy string         `json:"updatedBy"`
+	Articles  []*Article     `gorm:"many2many:article_tag" json:"articles"`
 }
